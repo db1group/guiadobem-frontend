961d9e737e77910711bbc6cd08e5e34440df9d04
Edson Caliman <edsonjunior@db1.com.br>
2020-04-19 17:50:44 -0300

    Definida mensagem para quando não tiver estabelecimentos

--- src/componentes/ListaEstabelecimentos.js
+++ src/componentes/ListaEstabelecimentos.js
@@ -1,61 +1,60 @@
 import React, { Component } from "react";
 import { Container, CardColumns, Card, Row, Form } from "react-bootstrap";
-import { FaPhone, FaUser, FaBarcode, FaWhatsapp } from 'react-icons/fa';
-import { possuiTexto, textoIgual, removerAcentos } from '../util/utilidade';
+import { FaPhone, FaUser, FaBarcode, FaWhatsapp } from "react-icons/fa";
+import { possuiTexto, textoIgual, removerAcentos } from "../util/utilidade";
 import api from "../services/api";
 import Cabecalho from "./Cabecalho";
 import Titulo from "./Titulo";
+import imgTriste from "../imagens/cara-triste.png";
 
 import "./ListaEstabelecimento.css";
 
 export default class ListaEstabelecimentos extends Component {
-
-  PLANOFUNDO = [
-    "bkgHortiFruiti", 
-    "bkgConfeitaria", 
-    "bkgServicos", 
-    "bkgOutros"
-  ]
+  PLANOFUNDO = ["bkgHortiFruiti", "bkgConfeitaria", "bkgServicos", "bkgOutros"];
 
   TIPOSTEXTO = {
     telefone: {
       icone: <FaPhone />,
-      rotulo: 'Telefone',
+      rotulo: "Telefone",
     },
     responsavel: {
       icone: <FaUser />,
-      rotulo: 'Responsável',
+      rotulo: "Responsável",
     },
     tipoproduto: {
       icone: <FaBarcode />,
-      rotulo: 'Tipo de produto',
+      rotulo: "Tipo de produto",
     },
     whatsapp: {
       icone: <FaWhatsapp />,
-      rotulo: 'Whatsapp',
-    }
-  }
+      rotulo: "Whatsapp",
+    },
+  };
 
-  state = {  
+  state = {
     estabelecimentos: [],
     estabelecimentosFiltrados: [],
     textoTitulo: "",
     textoPesquisado: "",
     idCidade: "",
     idCategoria: "",
-  }
+  };
 
   async componentDidMount() {
-    const {idcidade, idcategoria} = this.props.match.params;
+    const { idcidade, idcategoria } = this.props.match.params;
     const responseCategoria = await api.get(`/categorias/${idcategoria}`);
-    const responseCidade = await api.get(`/cidades/${idcidade}`);   
+    const responseCidade = await api.get(`/cidades/${idcidade}`);
     const textoTitulo = `${responseCidade.data.nome} - ${responseCategoria.data.nome}`;
 
     this.setState({
-      textoTitulo: `${responseCidade.data.nome ? textoTitulo : responseCategoria.data.nome} ` 
-    });    
+      textoTitulo: `${
+        responseCidade.data.nome ? textoTitulo : responseCategoria.data.nome
+      } `,
+    });
 
-    const response = await api.get(`/estabelecimentos/${idcidade}/${idcategoria}`);
+    const response = await api.get(
+      `/estabelecimentos/${idcidade}/${idcategoria}`
+    );
     this.setState({
       estabelecimentos: response.data,
     });
@@ -65,13 +64,13 @@ export default class ListaEstabelecimentos extends Component {
     const { estabelecimentos } = this.state;
 
     if (prevState.estabelecimentos !== estabelecimentos) {
-        this.atualizarEstabelecimentosFiltrados();
+      this.atualizarEstabelecimentosFiltrados();
     }
   }
 
   atualizarEstabelecimentosFiltrados = () => {
-    const { textoPesquisado, estabelecimentos } = this.state;    
-    
+    const { textoPesquisado, estabelecimentos } = this.state;
+
     if (!textoPesquisado || textoPesquisado === "") {
       this.setState({
         estabelecimentosFiltrados: estabelecimentos,
@@ -80,20 +79,22 @@ export default class ListaEstabelecimentos extends Component {
       return;
     }
 
-    const estabelecimentosFiltrados = estabelecimentos.filter(estabelecimento => {
-        return possuiTexto(estabelecimento.nome, textoPesquisado)
-          || possuiTexto(estabelecimento.tipo, textoPesquisado)
-          || possuiTexto(estabelecimento.telefone, textoPesquisado)
-          || possuiTexto(estabelecimento.whatsapp, textoPesquisado)
-          || possuiTexto(estabelecimento.responsavel, textoPesquisado);
-    });
-    
+    const estabelecimentosFiltrados = estabelecimentos.filter(
+      (estabelecimento) => {
+        return (
+          possuiTexto(estabelecimento.nome, textoPesquisado) ||
+          possuiTexto(estabelecimento.tipo, textoPesquisado) ||
+          possuiTexto(estabelecimento.telefone, textoPesquisado) ||
+          possuiTexto(estabelecimento.whatsapp, textoPesquisado) ||
+          possuiTexto(estabelecimento.responsavel, textoPesquisado)
+        );
+      }
+    );
+
     this.setState({
       estabelecimentosFiltrados: estabelecimentosFiltrados,
-    })  
-  }
-
-  
+    });
+  };
 
   render() {
     const { history } = this.props;
@@ -103,11 +104,10 @@ export default class ListaEstabelecimentos extends Component {
       <Container>
         <Cabecalho />
         <Container>
-            <Titulo botaoVoltarClique={history.goBack}>
-              {textoTitulo}
-            </Titulo>
-            {this.renderFiltro()}
+          <Titulo botaoVoltarClique={history.goBack}>{textoTitulo}</Titulo>
+          {this.renderFiltro()}
         </Container>
+        {this.renderSemEstabelecimentos()}
         <CardColumns className="cardColumnsEstabelecimentos">
           {estabelecimentosFiltrados.map((estabelecimento) => (
             <Card
@@ -116,7 +116,9 @@ export default class ListaEstabelecimentos extends Component {
               className={this.definirPlanoFundo()}
             >
               <Card.Body>
-                <Card.Title>{this.renderTitulo(estabelecimento.nome)}</Card.Title>
+                <Card.Title>
+                  {this.renderTitulo(estabelecimento.nome)}
+                </Card.Title>
                 {this.renderCorpo(estabelecimento)}
               </Card.Body>
             </Card>
@@ -126,94 +128,147 @@ export default class ListaEstabelecimentos extends Component {
     );
   }
 
-  renderFiltro = () => (
-      <Form>
-        <Row>
-          <Form.Control as="input" 
-                        id="filtro-estabelecimentos" 
-                        type="text"
-                        name="filtro-estabelecimentos"
-                        placeholder="Filtrar estabelecimentos"
-                        autoComplete="off"
-                        onChange={this.mudarPesquisa} 
-          />
-        </Row>
-      </Form>
-  )
+  renderFiltro = () => {
+    const { estabelecimentos } = this.state;
+    if (estabelecimentos.length > 0) {
+      return (
+        <Form>
+          <Row>
+            <Form.Control
+              as="input"
+              id="filtro-estabelecimentos"
+              type="text"
+              name="filtro-estabelecimentos"
+              placeholder="Filtrar estabelecimentos"
+              autoComplete="off"
+              onChange={this.mudarPesquisa}
+            />
+          </Row>
+        </Form>
+      );
+    }
+  };
 
   mudarPesquisa = (event, valid) => {
     const { value } = event.target;
-    this.setState({
+    this.setState(
+      {
         textoPesquisado: value,
-    }, () => {
+      },
+      () => {
         this.atualizarEstabelecimentosFiltrados();
-    })
-  }
+      }
+    );
+  };
 
   definirPlanoFundo = () => {
     const { idcategoria } = this.props.match.params;
-    return this.PLANOFUNDO[idcategoria-1]    
+    return this.PLANOFUNDO[idcategoria - 1];
   };
 
   renderTitulo = (texto) => {
     const { textoPesquisado } = this.state;
     return (
-        <span style={{fontWeight:"bold"}}>{this.destacarTexto(texto, textoPesquisado)}</span>
-    )
-  }
+      <span style={{ fontWeight: "bold" }}>
+        {this.destacarTexto(texto, textoPesquisado)}
+      </span>
+    );
+  };
 
   renderCorpo = (estabelecimento) => {
-
     return (
       <>
-        <div className="tipo-texto-card">{this.renderTexto("tipoproduto", estabelecimento.tipo)}</div>
-        <div className="tipo-texto-card">{this.renderTexto("telefone", estabelecimento.telefone)}</div>
-        <div className="tipo-texto-card">{this.renderTexto("whatsapp", estabelecimento.whatsapp)}</div>
-        <div className="tipo-texto-card">{this.renderTexto("responsavel", estabelecimento.responsavel)}</div>
+        <div className="tipo-texto-card">
+          {this.renderTexto("tipoproduto", estabelecimento.tipo)}
+        </div>
+        <div className="tipo-texto-card">
+          {this.renderTexto("telefone", estabelecimento.telefone)}
+        </div>
+        <div className="tipo-texto-card">
+          {this.renderTexto("whatsapp", estabelecimento.whatsapp)}
+        </div>
+        <div className="tipo-texto-card">
+          {this.renderTexto("responsavel", estabelecimento.responsavel)}
+        </div>
       </>
-    )
-  }
+    );
+  };
   renderTexto = (tipo, texto) => {
     const { textoPesquisado } = this.state;
 
     if (!texto || !tipo) {
-      return undefined
+      return undefined;
     }
 
     const tipoTexto = this.TIPOSTEXTO[tipo];
     return (
       <div>
-        <div>{tipoTexto.icone} <span style={{fontWeight:"bold"}}>{tipoTexto.rotulo}</span></div>
+        <div>
+          {tipoTexto.icone}{" "}
+          <span style={{ fontWeight: "bold" }}>{tipoTexto.rotulo}</span>
+        </div>
         <div>{this.destacarTexto(texto, textoPesquisado)}</div>
       </div>
-    )
-  }
-  
-  destacarTexto = (texto, destaque) => {
-      if (!texto || !destaque) {
-          return texto;
-      }
-      
-      const textoSemAcentos = removerAcentos(texto).toLowerCase();
-      const destaqueSemAcentos = removerAcentos(destaque).toLowerCase();      
-      const partesSemAcento = textoSemAcentos.split(new RegExp(`(${destaqueSemAcentos})`, 'gi'));
-      
-      let indiceAtual = 0;
-      const partes = partesSemAcento.map(
-        (parteSemAcento) => {
-          const textoOriginal = texto.substr(indiceAtual, parteSemAcento.length);
-          indiceAtual += parteSemAcento.length;
-          return textoOriginal; 
-        } 
-      );
+    );
+  };
 
+  destacarTexto = (texto, destaque) => {
+    if (!texto || !destaque) {
+      return texto;
+    }
+
+    const textoSemAcentos = removerAcentos(texto).toLowerCase();
+    const destaqueSemAcentos = removerAcentos(destaque).toLowerCase();
+    const partesSemAcento = textoSemAcentos.split(
+      new RegExp(`(${destaqueSemAcentos})`, "gi")
+    );
+
+    let indiceAtual = 0;
+    const partes = partesSemAcento.map((parteSemAcento) => {
+      const textoOriginal = texto.substr(indiceAtual, parteSemAcento.length);
+      indiceAtual += parteSemAcento.length;
+      return textoOriginal;
+    });
+
+    return (
+      <>
+        {partes.map((parte, index) =>
+          textoIgual(parte, destaque) ? (
+            <span
+              key={index}
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "rgba(201, 76, 76, 0.3)",
+              }}
+            >
+              {parte}
+            </span>
+          ) : (
+            parte
+          )
+        )}
+      </>
+    );
+  };
+
+  renderSemEstabelecimentos = () => {
+    const { estabelecimentos } = this.state;
+    if (estabelecimentos.length === 0) {
       return (
-        <>
-          {partes.map(
-            (parte, index) => (textoIgual(parte, destaque)) 
-              ? <span key={index} style={{fontWeight: "bold", backgroundColor: "rgba(201, 76, 76, 0.3)"}}>{parte}</span> 
-              : parte
-            )}
-        </>);
-  }
+        <Container className="texto-sem-cadastro">
+          <img src={imgTriste} alt="Triste sem cadastros" />
+          <span>
+            <h6>
+              Poxa, que pena, não há nenhum cadastro para esta categoria por
+              aqui!
+            </h6>
+            <br></br>
+            Mas você pode mudar isso: ajude, indique e cadastre seu comércio ou
+            o comércio de seu vizinho, e ajude a deixar este projeto ainda mais
+            completo!
+          </span>
+        </Container>
+      );
+    }
+  };
 }
