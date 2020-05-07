2682d3bfc8245b90ca11bdbd8e56b91e7553057f
Edson Caliman <edsonjunior@db1.com.br>
2020-05-07 08:41:10 -0300

    Adicionar google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import ReactGA from "react-ga";
 import "./App.css";
 import Routes from "./Routes";
 
+function initializeAnalytics() {
+  ReactGA.initialize("UA-165847257-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 function App() {
+  initializeAnalytics();
   return <Routes />;
 }
 
