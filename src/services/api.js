feb402f3de6dbc5ec339916698a5939ae125cc55
Edson Caliman <edsonjunior@db1.com.br>
2020-05-13 17:58:33 -0300

    Troca do servidor de back end

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-54-89-248-30.compute-1.amazonaws.com:8080/api/public/"
+  baseURL: "/api/public/",
 });
 
 export default api;
